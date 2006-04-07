b7d281dea16e6199bca275bf97a0aa64b2644c37
Adam Jackson <ajax@nwnk.net>
2006-04-07 17:14:28 +0000

    Unlibcwrap. Bump server version requirement. Bump to 1.1.0.

--- src/xf86DMC.c
+++ src/xf86DMC.c
@@ -46,12 +46,11 @@
 #include <misc.h>
 #include <xf86.h>
 #define NEED_XF86_TYPES
-#include <xf86_ansic.h>
 #include <xf86_OSproc.h>
 #include <xf86Xinput.h>
 #include <xisb.h>
 #include <exevents.h>
-
+#include <string.h>
 #include "xf86DMC.h"
 
 _X_EXPORT InputDriverRec DMC = {
@@ -75,7 +74,7 @@ static XF86ModuleVersionInfo VersionRec =
 	MODINFOSTRING1,
 	MODINFOSTRING2,
 	XORG_VERSION_CURRENT,
-	1, 0, 0,
+	1, 1, 0,
 	ABI_CLASS_XINPUT,
 	ABI_XINPUT_VERSION,
 	MOD_CLASS_XINPUT,
